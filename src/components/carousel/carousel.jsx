9520fa5bdd58bee59ee4701c9e95ddb27855a1e9
Kavinesh G <indranigk@gmail.com>
2024-07-08 16:50:47 +0530

    com

--- src/components/carousel/carousel.jsx
+++ src/components/carousel/carousel.jsx
@@ -1,105 +1,63 @@
-import React, { useEffect, useState } from "react";
-import { motion, useMotionValue } from "framer-motion";
+import React, { useEffect } from "react";
+import "./carousel.css";
+import { assets } from "../../assets/assets";
+import Image1 from './b1.jpg';
+import Image2 from './b2.jpg';
 
-const imgs = [
-  "./b1.jpg",
-  "./b2.jpg",
-];
-
-const ONE_SECOND = 1000;
-const AUTO_DELAY = ONE_SECOND * 10;
-const DRAG_BUFFER = 50;
-
-const SPRING_OPTIONS = {
-  type: "spring",
-  mass: 3,
-  stiffness: 400,
-  damping: 50,
-};
-
-export const SwipeCarousel = () => {
-  const [imgIndex, setImgIndex] = useState(0);
-  const dragX = useMotionValue(0);
-
+const Carousel = () => {
   useEffect(() => {
-    const intervalRef = setInterval(() => {
-      const x = dragX.get();
-      if (x === 0) {
-        setImgIndex((pv) => (pv === imgs.length - 1 ? 0 : pv + 1));
-      }
-    }, AUTO_DELAY);
+    autoSlide();
+  }, []);
 
-    return () => clearInterval(intervalRef);
-  }, [dragX]);
+  const autoSlide = () => {
+    setInterval(() => {
+      slide(getItemActiveIndex() + 1);
+    }, 3000); // slide speed = 3s
+  };
 
-  const onDragEnd = () => {
-    const x = dragX.get();
-    if (x <= -DRAG_BUFFER && imgIndex < imgs.length - 1) {
-      setImgIndex((pv) => pv + 1);
-    } else if (x >= DRAG_BUFFER && imgIndex > 0) {
-      setImgIndex((pv) => pv - 1);
+  const slide = (toIndex) => {
+    const itemsArray = Array.from(document.querySelectorAll(".carousel_item"));
+    const itemActive = document.querySelector(".carousel_item__active");
+
+    if (toIndex >= itemsArray.length) {
+      toIndex = 0;
     }
+
+    const newItemActive = itemsArray[toIndex];
+
+    newItemActive.classList.add("carousel_item__pos_next");
+    setTimeout(() => {
+      newItemActive.classList.add("carousel_item__next");
+      itemActive.classList.add("carousel_item__next");
+    }, 20);
+
+    newItemActive.addEventListener("transitionend", () => {
+      itemActive.className = "carousel_item";
+      newItemActive.className = "carousel_item carousel_item__active";
+    }, {
+      once: true
+    });
+  };
+
+  const getItemActiveIndex = () => {
+    const itemsArray = Array.from(document.querySelectorAll(".carousel_item"));
+    const itemActive = document.querySelector(".carousel_item__active");
+    const itemActiveIndex = itemsArray.indexOf(itemActive);
+    return itemActiveIndex;
   };
 
   return (
-    <div className="relative overflow-hidden bg-white py-4 w-full md:w-11/12 mx-auto">
-      <motion.div
-        drag="x"
-        dragConstraints={{ left: 0, right: 0 }}
-        style={{ x: dragX }}
-        animate={{ translateX: `-${imgIndex * 100}%` }}
-        transition={SPRING_OPTIONS}
-        onDragEnd={onDragEnd}
-        className="flex cursor-grab items-center active:cursor-grabbing px-3"
-      >
-        <Images imgIndex={imgIndex} />
-      </motion.div>
-      <Dots imgIndex={imgIndex} setImgIndex={setImgIndex} />
+    <div className="carousel">
+      <div className="carousel_inner">
+        <div className="carousel_item carousel_item__active">
+          <img src={Image1} alt="" className="carousel_img" />
+        </div>
+        <div className="carousel_item">
+          <img src={Image2} alt="" className="carousel_img" />
+        </div>
+      </div>
     </div>
   );
 };
 
-const Images = ({ imgIndex }) => {
-  return (
-    <>
-      {imgs.map((imgSrc, idx) => (
-        <motion.div
-          key={idx}
-          style={{
-            backgroundImage: `url(${imgSrc})`,
-            backgroundSize: "cover",
-            backgroundPosition: "center",
-          }}
-          animate={{ scale: imgIndex === idx ? 0.95 : 0.85 }}
-          transition={SPRING_OPTIONS}
-          className="aspect-video w-full h-80 md:h-[30rem] shrink-0 rounded-xl bg-neutral-800 object-cover"
-        />
-      ))}
-    </>
-  );
-};
-
-const Dots = ({ imgIndex, setImgIndex }) => {
-  return (
-    <div className="mt-2 md:mt-4 flex w-full justify-center gap-2">
-      {imgs.map((_, idx) => (
-        <button
-          key={idx}
-          onClick={() => setImgIndex(idx)}
-          className={`h-3 w-3 rounded-full transition-colors ${
-            idx === imgIndex ? "bg-neutral-50" : "bg-neutral-500"
-          }`}
-        />
-      ))}
-    </div>
-  );
-};
-
-export default SwipeCarousel;
-
-
-
-
-
-
-
+export default Carousel;
