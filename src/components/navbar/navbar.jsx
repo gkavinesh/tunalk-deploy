dfd9014042e2686ee586a3bcc9fb0287d20ec4b6
Kavinesh G <indranigk@gmail.com>
2024-07-16 01:48:54 +0530

    Commit-2

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,6 +5,7 @@ import { Link } from 'react-router-dom'
 import { StoreContext } from '../../context/StoreContext'
 import { FaUser, FaShoppingCart, FaSearch } from 'react-icons/fa';
 import { MotionConfig, motion, AnimatePresence } from "framer-motion";
+import { FaPhoneAlt } from "react-icons/fa";
 import { FaHome, FaInfoCircle, FaProductHunt, FaEnvelope, FaQuestionCircle, FaShieldAlt } from 'react-icons/fa'
 
 const AnimatedHamburgerButton = () => {
@@ -90,31 +91,31 @@ const Navbar = ({ setShowLogin }) => {
     };
 
     return (
-            <div className='navbar' id='navbar'>
-                <a href="/">
-                    <img src={assets.white} alt="" className="logo" />
-                </a>
+        <div className='navbar' id='navbar'>
+            <a href="/">
+                <img src={assets.white} alt="" className="logo" />
+            </a>
 
-                <div className="navbar-right">
-                    <button className='navbar-button-2'>
-                        <FaSearch style={{ marginRight: '10px', marginTop: '4px' }} />
-                        Search
-                    </button>
-                    <button onClick={() => setShowLogin(true)} className='navbar-button'>
-                        <FaUser style={{ marginRight: '10px', marginTop: '4px' }} />
-                        Sign in
-                    </button>
-                    <div className="navbar-search-icon">
-                        <Link to="/cart" className="navbar-cart-link">
-                            <FaShoppingCart style={{ marginRight: '10px', marginTop: '4px' }} />
-                            <span className="navbar-cart-text">Cart</span>
-                            <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
-                        </Link>
-                    </div>
-                    <div className='navbar-hamburger' onClick={toggleSidebar}>
-                        <AnimatedHamburgerButton />
-                    </div>
+            <div className="navbar-right">
+                <button className='navbar-button-2'>
+                    <FaSearch style={{ marginRight: '10px', marginTop: '4px' }} />
+                    Search
+                </button>
+                <button onClick={() => setShowLogin(true)} className='navbar-button'>
+                    <FaUser style={{ marginRight: '10px', marginTop: '4px' }} />
+                    Sign in
+                </button>
+                <div className="navbar-search-icon">
+                    <Link to="/cart" className="navbar-cart-link">
+                        <FaShoppingCart style={{ marginRight: '10px', marginTop: '4px' }} />
+                        <span className="navbar-cart-text">Cart</span>
+                        <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
+                    </Link>
                 </div>
+                <div className='navbar-hamburger' onClick={toggleSidebar}>
+                    <AnimatedHamburgerButton />
+                </div>
+            </div>
 
 
             <AnimatePresence>
@@ -126,46 +127,46 @@ const Navbar = ({ setShowLogin }) => {
                         transition={{ duration: 0.5 }}
                         className="sidebar"
                     >
-                        <div className="flex justify-between items-center p-4">
+                        <div className="flex justify-between items-center">
                             <div className="sidebarlogo">
-                                <img src={assets.tunamain} alt="Logo" className="sidebar-logo-img" />
+                                <h2>TunaLK</h2>
                             </div>
                         </div>
                         <ul className="menu-list p-4">
                             <div className='icons'>
-                                <FaHome className="menu-icon" />
-                                <a href='#products'>Home</a>
+                                <a href='#home'>Home</a>
                             </div>
+                            <br></br>
                             <div className='icons'>
-                                <FaInfoCircle className="menu-icon" />
                                 <a href='#about'>About</a>
                             </div>
+                            <br></br>
                             <div className='icons'>
-                                <FaProductHunt className="menu-icon" />
-                                <a href='#footer'>Products</a>
+                                <a href='#products'>Products</a>
                             </div>
+                            <br></br>
                             <div className='icons'>
-                            <FaEnvelope className="menu-icon" />
-                                <a href='#products'>Contact</a>
-                                </div>
+                                <a href='#contact'>Contact</a>
+                            </div>
+                            <br></br>
                             <div className='icons'>
-                            <FaQuestionCircle className="menu-icon" />
-                                <a href='#about'>FAQ</a></div>
+                                <a href='#faq'>FAQ</a>
+                            </div>
+                            <br></br>
                             <div className='icons'>
-                            <FaShieldAlt className="menu-icon" />
-                                <a href='#footer'>Privacy Policy</a></div>
+                                <a href='#privacy-policy'>Privacy Policy</a>
+                            </div>
                         </ul>
-                        <div className="flex justify-between items-center p-4">
-                            <div className="downloadlogo">
-                                <img src={assets.app_store} alt="Logo" className="sidebar-logo-img" />
-                            </div>
-                            <div className="downloadlogo">
-                                <img src={assets.play_store} alt="Logo" className="sidebar-logo-img" />
-                            </div>
-                        </div>
+                        <button className="call-button-2">
+                            Download our App
+                        </button>
+                        <button className="call-button">
+                            Contact US
+                        </button>
                     </motion.div>
                 )}
             </AnimatePresence>
+
         </div>
     );
 }
