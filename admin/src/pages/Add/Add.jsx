702f9587d7a56de88ff932dfdef6588a44b5e37e
Kavinesh G <indranigk@gmail.com>
2024-07-19 00:14:07 +0530

    Commit-1

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,25 +1,46 @@
-import React from 'react'
-import './Add.css'
-import { assets } from '../../assets/assets'
+import React, { useState } from 'react';
+import './Add.css';
+import { assets } from '../../assets/assets';
 
 const Add = () => {
+
+  const [image,setImage] = useState(false)
+  const [data,setData] = useState({
+    name:"",
+    description:"",
+    price:"",
+    category:"Fish"
+  })
+
+  const onChangeHandler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData(data=>({...data,[name]:value}))
+  }
+
+  const onSubmitHandler = async (event) => {
+
+  }
+
+
+
   return (
     <div className='add'>
-      <form className='flex-col'>
+      <form className='flex-col' onSubmit={onSubmitHandler} >
         <div className="add-img-upload flex-col">
           <p>Upload Image</p>
           <label htmlFor="image">
-            <img src={assets.upload_area} alt=''/>
+            <img src={image?URL.createObjectURL(image):assets.upload_area} alt=''/>
           </label>
-          <input type='file' id='image' hidden required/>
+          <input onChange={(e)=>setImage(e.target.files[0])} type='file' id='image' hidden required/>
         </div>
         <div className="add-product-name flex-col">
           <p>Product Name</p>
-          <input type='text' name='name' placeholder='Type here'/>
+          <input onChange={onChangeHandler} value={data.name} type='text' name='name' placeholder='Type here'/>
         </div>
         <div className='add-product-description flex-col'>
           <p>Product Description</p>
-          <textarea name="description" rows="6" placeholder="Write content here"/>
+          <textarea onChange={onChangeHandler} value={data.description} name="description" rows="6" placeholder="Write content here"/>
         </div>
         <div className="add-category-price">
           <div className='add-category flex-col'>
@@ -34,7 +55,7 @@ const Add = () => {
           </div>
           <div className="add-price flex-col">
             <p>Product Price</p>
-            <input type="Number" name="price" placeholder="$20"/>
+            <input onChange={onChangeHandler} value={data.price} type="Number" name="price" placeholder="$20"/>
           </div>
         </div>
         <button type="submit" className="add-btn">ADD</button>
