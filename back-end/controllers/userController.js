ce52d885f520e848715b47a9b8f39b948bc23e01
Kavinesh G <indranigk@gmail.com>
2024-07-22 03:10:25 +0530

    commit

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -12,7 +12,33 @@ const loginUser = async (req,res) => {
 // register user
 
 const registerUser = async (req,res) => {
+    const {name,password,email} = req.body;
+    try {
+        //checking if user already exists
+        const exists = await userModel.findOne({email});
+        if(exists){
+            return res,json({success:false,message:"User already exists."})
+        }
 
+        //validating email format and strong password
+        if(!validator.isEmail(email)){
+            return res.json({success:false,message:"Please enter a valid email."})
+        }
+
+        //enter strong password
+        if(password.length < 8){
+            return res.json({success:false,message:"Please enter a stronger pasword"})
+        }
+
+        //hashing user password
+        const salt = await bcrypt.genSalt(10)
+        const hashedPassword = await bcrypt.hash(password,salt)
+
+        const newUser = new userModel()
+
+    } catch (error) {
+        
+    }
 }
 
 export {loginUser,registerUser}
