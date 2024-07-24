85f2769ddeb2d5d80a3f1a4e7a95c6481ccbfddf
Kavinesh G <indranigk@gmail.com>
2024-07-24 13:41:56 +0530

    commit

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -2,6 +2,8 @@ import React, { useEffect, useRef } from "react";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
 import "./about.css";
+import {  EyeIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
+import Image from './bg-fish.png'
 
 import About from '..//..//assets/About_wallpaper.png'
 
@@ -49,6 +51,90 @@ const App = () => {
 
   return (
     <div className="wrapper" ref={wrapperRef} >
+       <div className="relative isolate overflow-hidden bg-white px-6 py-24 sm:py-32 lg:overflow-visible lg:px-0 tunabub">
+      <div className="absolute inset-0 -z-10 overflow-hidden">
+        <svg
+          aria-hidden="true"
+          className="absolute left-[max(50%,25rem)] top-0 h-[64rem] w-[128rem] -translate-x-1/2 stroke-gray-200 [mask-image:radial-gradient(64rem_64rem_at_top,white,transparent)]"
+        >
+          <defs>
+            <pattern
+              x="50%"
+              y={-1}
+              id="e813992c-7d03-4cc4-a2bd-151760b470a0"
+              width={200}
+              height={200}
+              patternUnits="userSpaceOnUse"
+            >
+              <path d="M100 200V.5M.5 .5H200" fill="none" />
+            </pattern>
+          </defs>
+          <svg x="50%" y={-1} className="overflow-visible fill-gray-50">
+            <path
+              d="M-100.5 0h201v201h-201Z M699.5 0h201v201h-201Z M499.5 400h201v201h-201Z M-300.5 600h201v201h-201Z"
+              strokeWidth={0}
+            />
+          </svg>
+          <rect fill="url(#e813992c-7d03-4cc4-a2bd-151760b470a0)" width="100%" height="100%" strokeWidth={0} />
+        </svg>
+      </div>
+      <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 lg:mx-0 lg:max-w-none lg:grid-cols-2 lg:items-start lg:gap-y-10">
+        <div className="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+          <div className="lg:pr-4">
+            <div className="lg:max-w-lg">
+              <p className="text-base font-semibold leading-7 text-teal-500">About</p>
+              <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">We are Tuna LK</h1>
+              <p className="mt-6 text-xl leading-8 text-gray-700">
+              At TunaLK, we don't just sell seafood; we offer a journey into the heart of the ocean, bringing its freshest treasures right to your table.
+              </p>
+            </div>
+          </div>
+        </div>
+          <img
+            alt=""
+            src={Image}
+            className="w-[24rem] max-w-none bg-transparent ring-gray-400/10 sm:w-[57rem]"
+          />
+        <div className="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
+          <div className="lg:pr-4">
+            <div className="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
+              <p>
+              What sets TunaLK apart is our unwavering commitment to delivering the best. Our seafood is not only fresh but also full of flavor, capturing the essence of the ocean in every bite. We invite you to discover the difference that quality and care make. Experience the superior taste and exceptional service that only TunaLK can provide.
+              </p>
+              <ul role="list" className="mt-8 space-y-8 text-gray-600">
+                <li className="flex gap-x-3">
+                <EyeIcon aria-hidden="true" className="mt-1 h-5 w-5 flex-none text-teal-600" />
+
+                  <span>
+                    <strong className="font-semibold text-gray-900">Our Vision</strong> To be the premier online destination for fresh and sustainable seafood, renowned for our commitment to quality, customer satisfaction, and environmental stewardship.
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <LockClosedIcon aria-hidden="true" className="mt-1 h-5 w-5 flex-none text-indigo-600" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Our Mission</strong> Our mission is to provide customers with the freshest, highest-quality seafood sourced from sustainable fisheries and aquaculture. We aim to enhance the culinary experience by offering a diverse selection of products, coupled with exceptional service and convenient online shopping.
+                  </span>
+                </li>
+                <li className="flex gap-x-3">
+                  <ServerIcon aria-hidden="true" className="mt-1 h-5 w-5 flex-none text-indigo-600" />
+                  <span>
+                    <strong className="font-semibold text-gray-900">Our Values</strong> We are dedicated to delivering the finest seafood by maintaining rigorous standards throughout our supply chain. Our priority is environmentally responsible practices to ensure the long-term health of marine ecosystems. We strive to exceed customer expectations through personalized service, reliability, and transparency. Operating with honesty, we uphold the highest ethical standards in all our dealings. Continuously seeking innovation, we aim to improve our offerings and processes to provide an outstanding shopping experience.
+                  </span>
+                </li>
+              </ul>
+              <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">No server? No problem.</h2>
+              <p className="mt-6">
+                Id orci tellus laoreet id ac. Dolor, aenean leo, ac etiam consequat in. Convallis arcu ipsum urna nibh.
+                Pharetra, euismod vitae interdum mauris enim, consequat vulputate nibh. Maecenas pellentesque id sed
+                tellus mauris, ultrices mauris. Tincidunt enim cursus ridiculus mi. Pellentesque nam sed nullam sed diam
+                turpis ipsum eu a sed convallis diam.
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div className="tunabubu">
       <div className="content">
         <section className="section hero" ref={heroSectionRef}>
         </section>
@@ -62,6 +148,7 @@ const App = () => {
           height="1500px" // Adjust height as needed
       />
       </div>
+    </div>
     </div>
   );
 };
