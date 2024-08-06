446f1e47445234c699633e87ffb403c7024630e7
Kavinesh G <indranigk@gmail.com>
2024-08-06 12:18:31 +0530

    admin-upgrade

--- admin/src/App.jsx
+++ admin/src/App.jsx
@@ -1,51 +1,36 @@
-import React from 'react';
+import React from 'react'
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import Navbar from './components/Navbar/Navbar';
-import Sidebar from './components/Sidebar/Sidebar';
-import { Routes, Route, Navigate } from 'react-router-dom';
-import Add from "./pages/Add/Add";
-import Order from "./pages/Orders/Orders";
-import List from "./pages/List/List";
-import Payment from "./pages/Payments/payment";
-import Login from "./pages/Login/Login";
+import Navbar from './components/Navbar/Navbar'
+import Sidebar from './components/Sidebar/Sidebar'
+import {Routes,Route} from 'react-router-dom'
+import Add from "./pages/Add/Add"
+import Order from "./pages/Orders/Orders"
+import List from "./pages/List/List"
+import Payment from "./pages/Payments/payment"
+import Login from "./pages/Login/Login"
 
 const App = () => {
-  const url = "http://localhost:4000";
 
-  const isAuthenticated = localStorage.getItem('isAuthenticated') === 'true';
+  const url = "https://tunalk-backend-53lo.onrender.com";
 
   return (
     <div>
-      <ToastContainer />
-      <Routes>
-        <Route path="/login" element={<Login />} />
-        {isAuthenticated ? (
-          <Route
-            element={
-              <>
-                <Navbar />
-                <div className='app-content'>
-                  <Sidebar />
-                  <Routes>
-                    <Route path='/add' element={<Add url={url} />} />
-                    <Route path='/list' element={<List url={url} />} />
-                    <Route path='/orders' element={<Order url={url} />} />
-                    <Route path='/payment' element={<Payment url={url} />} />
-                    <Route path="*" element={<Navigate to="/add" />} />
-                  </Routes>
-                </div>
-              </>
-            }
-          />
-        ) : (
-          <Route path="*" element={<Navigate to="/login" />} />
-        )}
-      </Routes>
+      <ToastContainer/>
+      <Navbar/>
+      <div className='app-content'>
+        <Sidebar/>
+        <Routes>
+          <Route path='/add' element={<Add url={url}/>}/>
+          <Route path='/list' element={<List url={url}/>}/>
+          <Route path='/orders' element={<Order url={url}/>}/>
+          <Route path='/payment' element={<Payment url={url}/>}/>
+        </Routes>
+      </div>
     </div>
-  );
-};
+  )
+}
 
-export default App;
+export default App
 
 
